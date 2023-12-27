815a4ab2aa95dcd2990efb884a2ee4a4ab803a84
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-12-27 19:29:07 +0600

    refactor config reading into a separate function

--- main.py
+++ main.py
@@ -9,53 +9,52 @@ import huggingface_hub
 import configparser
 
 
-config = configparser.ConfigParser()
+def read_config():
+    config = configparser.ConfigParser()
+    config_file_path = "config.ini"
 
-config_file_exists = os.path.exists("config.ini")
+    default_config = {
+        "API_KEY": os.getenv("API_KEY", ""),
+        "API_ENDPOINT": os.getenv(
+            "API_ENDPOINT",
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=",
+        ),
+        "TEMPERATURE": float(os.getenv("TEMPERATURE", 1.0)),
+        "TOP_P": float(os.getenv("TOP_P", 0.99)),
+        "OUT_DIR": os.getenv("OUT_DIR", "./textbooks"),
+        "COUNT_PER_FILE": int(os.getenv("COUNT_PER_FILE", 1000)),
+    }
 
-API_KEY = os.getenv("API_KEY", "")
-API_ENDPOINT = os.getenv(
-    "API_ENDPOINT",
-    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=",
-)
-TEMPERATURE = float(os.getenv("TEMPERATURE", 1.0))
-TOP_P = float(os.getenv("TOP_P", 0.99))
-OUT_DIR = os.getenv("OUT_DIR", "./textbooks")
-COUNT_PER_FILE = int(os.getenv("COUNT_PER_FILE", 1000))
+    if os.path.exists(config_file_path):
+        config.read(config_file_path)
+        for key in default_config:
+            default_config[key] = config.get(
+                "Gemini", key, fallback=default_config[key]
+            )
 
-if not os.path.exists(OUT_DIR):
-    os.makedirs(OUT_DIR)
+    if not os.path.exists(default_config["OUT_DIR"]):
+        os.makedirs(default_config["OUT_DIR"])
 
-if config_file_exists and "" in (API_KEY, API_ENDPOINT):
-    config.read("config.ini")
-    API_KEY = config.get("Gemini", "API_KEY", fallback="")
-    API_ENDPOINT = config.get(
-        "Gemini",
-        "API_ENDPOINT",
-        fallback="https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=",
-    )
+    with open(config_file_path, "w") as configfile:
+        config["Gemini"] = {key: str(default_config[key]) for key in default_config}
+        config.write(configfile)
 
-    if API_KEY == "":
+    if default_config["API_KEY"] == "":
         print("API key is empty. Please update config.ini with your API key.")
         exit(1)
 
-else:
-    config["Gemini"] = {
-        "API_KEY": API_KEY,
-        "API_ENDPOINT": API_ENDPOINT,
-    }
-    config["Parameters"] = {"TEMPERATURE": str(TEMPERATURE), "TOP_P": str(TOP_P)}
-    config["Misc"] = {
-        "OUT_DIR": OUT_DIR,
-        "COUNT_PER_FILE": str(COUNT_PER_FILE),
-    }
-    with open("config.ini", "w") as configfile:
-        config.write(configfile)
-
-    print(
-        "config.ini file not found. A default config.ini has been generated. Please update it with your API key and other configurations."
+    return (
+        default_config["API_KEY"],
+        default_config["API_ENDPOINT"],
+        float(default_config["TEMPERATURE"]),
+        float(default_config["TOP_P"]),
+        default_config["OUT_DIR"],
+        int(default_config["COUNT_PER_FILE"]),
     )
-    exit(1)
+
+
+API_KEY, API_ENDPOINT, TEMPERATURE, TOP_P, OUT_DIR, COUNT_PER_FILE = read_config()
+
 
 TEXT_DATASET = {
     "id": "WizardLM/WizardLM_evol_instruct_V2_196k",
