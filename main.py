851aa484f60e4012b3d73332d72b67d1275116b7
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-12-27 19:35:35 +0600

    refactor and standardize asynchronous data processing and file handling in the main function

--- main.py
+++ main.py
@@ -206,69 +206,82 @@ def load_iter_from_spec(spec):
 async def main():
     huggingface_hub.login(new_session=False)
     hf_api = huggingface_hub.HfApi()
+
     hf_user = hf_api.whoami()["name"]
-    hf_api.create_repo(
-        repo_id=f"{hf_user}/muse_textbooks", repo_type="dataset", exist_ok=True
-    )
+    repo_id = f"{hf_user}/muse_textbooks"
+
+    hf_api.create_repo(repo_id=repo_id, repo_type="dataset", exist_ok=True)
+
     text_iter = load_iter_from_spec(TEXT_DATASET)
     code_iter = load_iter_from_spec(CODE_DATASET)
     sess = aiohttp.ClientSession()
+
     tasks = set()
     lines = 0
+
     try:
-        with open(OUT_DIR + "/cur.jsonl", "rb") as f:
+        with open(os.path.join(OUT_DIR, "cur.jsonl"), "rb") as f:
             lines = len(f.read().decode("utf-8").split("\n")) - 1
     except Exception:
         pass
+
     pbar = tqdm(initial=lines, total=COUNT_PER_FILE)
-    outfile = open(OUT_DIR + "/cur.jsonl", "ab")
+    outfile = open(os.path.join(OUT_DIR, "cur.jsonl"), "ab")
+
     while True:
         template = TEMPLATES[secrets.randbits(64) % len(TEMPLATES)]
-        if template["dataset"] == "text":
-            passage = next(text_iter)
-        else:
-            passage = next(code_iter)
+        dataset_type = "text" if template["dataset"] == "text" else "code"
+        passage = next(text_iter) if dataset_type == "text" else next(code_iter)
+
         tasks.add(asyncio.create_task(llm_template_query(sess, template, passage)))
+
         new_tasks = set()
+
         for task in tasks:
             if not task.done():
                 new_tasks.add(task)
                 continue
+
             try:
                 result = task.result()
             except Exception as exc:
                 tqdm.write(f"failed: {exc}")
                 continue
+
             outfile.write((json.dumps({"text": result}) + "\n").encode("utf-8"))
             lines += 1
             pbar.update(1)
+
         tasks = new_tasks
+
         if lines >= COUNT_PER_FILE:
             outfile.close()
             i = 0
-            while True:
-                try:
-                    with open(OUT_DIR + f"/{i}.jsonl", "rb") as _:
-                        pass
-                except Exception:
-                    break
+
+            while os.path.exists(os.path.join(OUT_DIR, f"{i}.jsonl")):
                 i += 1
-            os.rename(OUT_DIR + "/cur.jsonl", OUT_DIR + f"/{i}.jsonl")
+
+            os.rename(
+                os.path.join(OUT_DIR, "cur.jsonl"), os.path.join(OUT_DIR, f"{i}.jsonl")
+            )
+
             while True:
                 try:
                     hf_api.upload_file(
-                        path_or_fileobj=OUT_DIR + f"/{i}.jsonl",
+                        path_or_fileobj=os.path.join(OUT_DIR, f"{i}.jsonl"),
                         path_in_repo=f"{i}.jsonl",
-                        repo_id=f"{hf_user}/muse_textbooks",
+                        repo_id=repo_id,
                         repo_type="dataset",
                     )
                     break
                 except Exception as exc:
                     tqdm.write(f"can't upload: {exc}")
                     await asyncio.sleep(1)
-            outfile = open(OUT_DIR + "/cur.jsonl", "ab")
+
+            outfile = open(os.path.join(OUT_DIR, "cur.jsonl"), "ab")
             lines = 0
             pbar.reset()
+
         await asyncio.sleep(1)
 
 
