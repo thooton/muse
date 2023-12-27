b27e116ac899e89f4c800537ddd316b3d9377e3e
Tanvir <tanvir.ahmed.tonoy@skiff.com>
2023-12-27 17:06:17 +0600

    feat: config reading support from env_var and config.ini

--- index.py
+++ index.py
@@ -6,14 +6,53 @@ import json
 from tqdm import tqdm
 import os
 import huggingface_hub
+import configparser
 
-# Get from https://makersuite.google.com/app/apikey
-GEMINI_API_KEY = "YOUR_API_KEY_HERE"
-GEMINI_BASE = "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key="
-TEMPERATURE = 1.0
-TOP_P = 0.99
-OUT_DIR = "./textbooks"
-COUNT_PER_FILE = 1000
+
+config = configparser.ConfigParser()
+
+config_file_exists = os.path.exists("config.ini")
+
+API_KEY = os.getenv("API_KEY", "")
+API_ENDPOINT = os.getenv(
+    "API_ENDPOINT",
+    "https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=",
+)
+TEMPERATURE = float(os.getenv("TEMPERATURE", 1.0))
+TOP_P = float(os.getenv("TOP_P", 0.99))
+OUT_DIR = os.getenv("OUT_DIR", "./textbooks")
+COUNT_PER_FILE = int(os.getenv("COUNT_PER_FILE", 1000))
+
+if config_file_exists and "" in (API_KEY, API_ENDPOINT):
+    config.read("config.ini")
+    API_KEY = config.get("Gemini", "API_KEY", fallback="")
+    API_ENDPOINT = config.get(
+        "Gemini",
+        "API_ENDPOINT",
+        fallback="https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=",
+    )
+
+    if API_KEY == "":
+        print("API key is empty. Please update config.ini with your API key.")
+        exit(1)
+
+else:
+    config["Gemini"] = {
+        "API_KEY": API_KEY,
+        "API_ENDPOINT": API_ENDPOINT,
+    }
+    config["Parameters"] = {"TEMPERATURE": str(TEMPERATURE), "TOP_P": str(TOP_P)}
+    config["Misc"] = {
+        "OUT_DIR": OUT_DIR,
+        "COUNT_PER_FILE": str(COUNT_PER_FILE),
+    }
+    with open("config.ini", "w") as configfile:
+        config.write(configfile)
+
+    print(
+        "config.ini file not found. A default config.ini has been generated. Please update it with your API key and other configurations."
+    )
+    exit(1)
 
 TEXT_DATASET = {
     "id": "WizardLM/WizardLM_evol_instruct_V2_196k",
@@ -128,7 +167,7 @@ The section will be surrounded by <section_42></section_42> tags.
 async def llm_query(sess, query):
     assert 0.0 <= TEMPERATURE <= 1.0
     assert 0.0 <= TOP_P <= 1.0
-    async with sess.post(GEMINI_BASE+GEMINI_API_KEY, json={
+    async with sess.post(API_ENDPOINT+API_KEY, json={
         "contents": [{
             "role": "USER",
             "parts": [{
